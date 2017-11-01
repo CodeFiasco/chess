79918c514f63b27a7d677ed9bd8c373da5767ec8
Francisco Machado <franm912@gmail.com>
2017-11-01 22:05:29 +0000

    king movement finished

--- src/org/academiadecodigo/chess/movable/piece/King.java
+++ src/org/academiadecodigo/chess/movable/piece/King.java
@@ -19,35 +19,39 @@ public class King extends Piece {
 
         // Top Row
         if (pos.getRow() > 0) {
-            if (pos.getCol() > 0) {
+            if (pos.getCol() > 0 && !grid.isFriendly(getPlayer(), pos.getCol() - 1, pos.getRow() - 1)) {
                 moves.add(new Position(pos.getCol() - 1, pos.getRow() - 1));
             }
 
+            if (!grid.isFriendly(getPlayer(), pos.getCol(), pos.getRow() - 1)) {
             moves.add(new Position(pos.getCol(), pos.getRow() - 1));
+            }
 
-            if (pos.getCol() < Constants.BOARD_SIZE - 1) {
+            if (pos.getCol() < Constants.BOARD_SIZE - 1 && !grid.isFriendly(getPlayer(), pos.getCol() + 1, pos.getRow() - 1)) {
                 moves.add(new Position(pos.getCol() + 1, pos.getRow() - 1));
             }
         }
 
         // Middle Row
-        if (pos.getCol() > 0) {
+        if (pos.getCol() > 0 && !grid.isFriendly(getPlayer(), pos.getCol() - 1, pos.getRow())) {
             moves.add(new Position(pos.getCol() - 1, pos.getRow()));
         }
 
-        if (pos.getCol() < Constants.BOARD_SIZE - 1) {
+        if (pos.getCol() < Constants.BOARD_SIZE - 1 && !grid.isFriendly(getPlayer(), pos.getCol() + 1, pos.getRow())) {
             moves.add(new Position(pos.getCol() + 1, pos.getRow()));
         }
 
         // Bottom Row
         if (pos.getRow() < Constants.BOARD_SIZE - 1) {
-            if (pos.getCol() > 0) {
+            if (pos.getCol() > 0 && !grid.isFriendly(getPlayer(), pos.getCol() - 1, pos.getRow() + 1)) {
                 moves.add(new Position(pos.getCol() - 1, pos.getRow() + 1));
             }
 
+            if (!grid.isFriendly(getPlayer(), pos.getCol(), pos.getRow() + 1)) {
             moves.add(new Position(pos.getCol(), pos.getRow() + 1));
+            }
 
-            if (pos.getCol() < Constants.BOARD_SIZE - 1) {
+            if (pos.getCol() < Constants.BOARD_SIZE - 1 && !grid.isFriendly(getPlayer(), pos.getCol() + 1, pos.getRow() + 1)) {
                 moves.add(new Position(pos.getCol() + 1, pos.getRow() + 1));
             }
         }
