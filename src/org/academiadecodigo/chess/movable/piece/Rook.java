6683cc0f818b4c2e7d5fe9516fc146eacc441cc4
Francisco Machado <franm912@gmail.com>
2017-11-01 21:28:00 +0000

    rook movement finished

--- src/org/academiadecodigo/chess/movable/piece/Rook.java
+++ src/org/academiadecodigo/chess/movable/piece/Rook.java
@@ -16,27 +16,68 @@ public class Rook extends Piece {
     @Override
     public Position[] possibleMoves() {
         List<Position> moves = new LinkedList<>();
+        boolean top = true, right = true, bottom = true, left = true;
 
         for (int i = 1; i < Constants.BOARD_SIZE; i++) {
-
             //TOP
-            if (pos.getRow() - i >= 0) {
-                moves.add(new Position(pos.getCol(), pos.getRow() - i));
+            if (top && pos.getRow() - i >= 0) {
+
+                if (grid.isOccupied(pos.getCol(), pos.getRow() - i)) {
+                    top = false;
+
+                    if (!grid.isFriendly(getPlayer(), pos.getCol(), pos.getRow() - i)) {
+                        moves.add(new Position(pos.getCol(), pos.getRow() - i));
+                    }
+                } else {
+
+                    moves.add(new Position(pos.getCol(), pos.getRow() - i));
+                }
             }
 
             // RIGHT
-            if (pos.getCol() + i < Constants.BOARD_SIZE) {
-                moves.add(new Position(pos.getCol() + i, pos.getRow()));
+            if (right && pos.getCol() + i < Constants.BOARD_SIZE) {
+
+                if (grid.isOccupied(pos.getCol() + i, pos.getRow())) {
+
+                    right = false;
+
+                    if (!grid.isFriendly(getPlayer(), pos.getCol() + i, pos.getRow())) {
+                        moves.add(new Position(pos.getCol() + i, pos.getRow()));
+                    }
+                } else {
+
+                    moves.add(new Position(pos.getCol() + i, pos.getRow()));
+                }
             }
 
             // BOTTOM
-            if (pos.getRow() + i < Constants.BOARD_SIZE) {
-                moves.add(new Position(pos.getCol(), pos.getRow() + i));
+            if (bottom && pos.getRow() + i < Constants.BOARD_SIZE) {
+
+                if (grid.isOccupied(pos.getCol(), pos.getRow() + i)) {
+                    bottom = false;
+
+                    if (grid.isFriendly(getPlayer(), pos.getCol(), pos.getRow() + i)) {
+                        moves.add(new Position(pos.getCol(), pos.getRow() + i));
+                    }
+                } else {
+
+                    moves.add(new Position(pos.getCol(), pos.getRow() + i));
+                }
             }
 
             // LEFT
-            if (pos.getCol() - i >= 0) {
-                moves.add(new Position(pos.getCol() - i, pos.getRow()));
+            if (left && pos.getCol() - i >= 0) {
+
+                if (grid.isOccupied(pos.getCol() - i, pos.getRow())) {
+                    left = false;
+
+                    if (grid.isFriendly(getPlayer(), pos.getCol() - i, pos.getRow())) {
+                        moves.add(new Position(pos.getCol() - i, pos.getRow()));
+                    }
+                } else {
+
+                    moves.add(new Position(pos.getCol() - i, pos.getRow()));
+                }
             }
         }
 
