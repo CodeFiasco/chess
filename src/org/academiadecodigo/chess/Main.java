b7f9065163b57f73eddcbc342b3e7503cf2536a1
Francisco Machado <franm912@gmail.com>
2017-11-01 15:03:41 +0000

    position

---
+++ src/org/academiadecodigo/chess/Main.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.chess;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/academiadecodigo/chess/Position/Position.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.chess.Position;
+
+public class Position {
+
+    private int col;
+    private int row;
+
+    public Position(int col, int row){
+        this.col = col;
+        this.row = row;
+    }
+}
